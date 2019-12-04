4f60fcd99be756d1b3aee7b672bb26b4abddd92b
nirifabien123 <nirifabien@gmail.com>
2019-12-04 23:54:36 +0200

    Last

--- Pages/Products/Index.cshtml.cs
+++ Pages/Products/Index.cshtml.cs
@@ -1,16 +1,12 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using ProductApp.Data;
 using ProductApp.Models;
 
-namespace ProductApp.Pages_Products
+namespace ProductApp.Pages.Products
 {
-    public class IndexModel : PageModel
+    public abstract class IndexModel : PageModel
     {
         private readonly ProductApp.Data.ProductContext _context;
 
